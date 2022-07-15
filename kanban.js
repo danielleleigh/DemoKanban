a9c294301f57ab56383162c984b399b7f20fc804
Danielle Allen <danielle.a.allen@accenture.com>
2022-07-15 11:25:57 -0400

    change code

--- kanban.js
+++ kanban.js
@@ -15,28 +15,25 @@ function drop(ev) {
 function createTask(){
     var x = document.getElementById("inprogress");
     var y = document.getElementById("done");
+    var w = document.getElementById("blocked");
+    var v = document.getElementById("onhold");
     var z = document.getElementById("create-new-task-block");
     if (x.style.display === "none") {
         x.style.display = "block";
         y.style.display = "block";
+        w.style.display = "block";
+        v.style.display = "block";
         z.style.display = "none";
     } else {
         x.style.display = "none";
         y.style.display = "none";
+        w.style.display = "none";
+        v.style.display = "none";
         z.style.display = "flex";
     }
 }
 
 function saveTask(){
-    // var saveButton = document.getElementById("save-button");
-    // var editButton = document.getElementById("edit-button");
-    // if (saveButton.style.display === "none") {
-    //     saveButton.style.display = "block";
-    //     editButton.style.display = "none";
-    // } else{
-    //     saveButton.style.display = "none";
-    //     editButton.style.display = "block";
-    // }
 
     var todo = document.getElementById("todo");
     var taskName = document.getElementById("task-name").value;
